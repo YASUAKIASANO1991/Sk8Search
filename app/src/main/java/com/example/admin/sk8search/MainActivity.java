00b03fa4196ca571d549c0ff5a2d0883d42282dd
康明 浅野 <yasuaki199181@outlook.com>
2018-09-06 15:19:59 +0900

    201809006 15:20 ファーストコミット

--- app/src/main/java/com/example/admin/sk8search/MainActivity.java
+++ app/src/main/java/com/example/admin/sk8search/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.admin.sk8search;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText strkeyword = (EditText)findViewById(R.id.txt_keyword);
+        Button btn_sarch = (Button)findViewById(R.id.btn_search);
+
+        btn_sarch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
